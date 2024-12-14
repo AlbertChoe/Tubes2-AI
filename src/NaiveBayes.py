c03735597c40f82f413328aad63f72ac97b573f3
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-12-14 23:56:18 +0700

    fix: fix nb

--- src/NaiveBayes.py
+++ src/NaiveBayes.py
@@ -3,51 +3,58 @@ import pandas as pd
 from typing import Dict, Any
 
 class NaiveBayes:
-    def fit(self, X_train: pd.DataFrame, y_train: pd.Series) -> None:
+    def fit(self, X_train: Any, y_train: Any) -> None:
         """Fit the model to the training data using Gaussian Naive Bayes."""
+        if isinstance(X_train, pd.DataFrame):
+            X_train = X_train.to_numpy()
+
+        if isinstance(y_train, pd.Series):
+            y_train = y_train.to_numpy()
+
         self.X_train = X_train
         self.y_train = y_train
         self.class_probs = self._calculate_class_probabilities(y_train)
         self.feature_params = self._calculate_feature_parameters(X_train, y_train)
+
     
-    def predict(self, X_test: pd.DataFrame) -> list:
+    def predict(self, X_test: np.ndarray) -> list:
         """Predict the class labels for the test data using Gaussian Naive Bayes."""
-        if isinstance(X_test, np.ndarray):
-            X_test = pd.DataFrame(X_test)
-        return [self._predict(x) for _, x in X_test.iterrows()]
+        if isinstance(X_test, pd.DataFrame):
+            X_test = X_test.to_numpy()
+        return [self._predict(x) for x in X_test]
     
-    def _predict(self, x: pd.Series) -> Any:
+    def _predict(self, x: np.ndarray) -> Any:
         """Predict the class label for a single test point using Gaussian Naive Bayes."""
         posteriors: Dict[Any, float] = {}
         
         for c in self.class_probs:
             prior = np.log(self.class_probs[c])
             likelihood = 0
-            for feature in x.index:
-                mean, var = self.feature_params[c].get(feature, (0, 1))
-                prob = self._gaussian_probability(x[feature], mean, var)
+            for i, feature_value in enumerate(x):
+                mean, var = self.feature_params[c].get(i, (0, 1))
+                prob = self._gaussian_probability(feature_value, mean, var)
                 likelihood += np.log(prob) if prob > 0 else -np.inf
             posteriors[c] = prior + likelihood
         
         return max(posteriors, key=posteriors.get)
 
-    def _calculate_class_probabilities(self, y_train: pd.Series) -> Dict[Any, float]:
+    def _calculate_class_probabilities(self, y_train: np.ndarray) -> Dict[Any, float]:
         """Calculate the prior probabilities of each class."""
         total_count = len(y_train)
-        class_counts = y_train.value_counts()
+        class_counts = pd.Series(y_train).value_counts()
         return (class_counts / total_count).to_dict()
     
-    def _calculate_feature_parameters(self, X_train: pd.DataFrame, y_train: pd.Series) -> Dict[Any, Dict[str, tuple]]:
+    def _calculate_feature_parameters(self, X_train: np.ndarray, y_train: np.ndarray) -> Dict[Any, Dict[int, tuple]]:
         """Calculate the mean and variance of features for each class."""
-        feature_params: Dict[Any, Dict[str, tuple]] = {}
+        feature_params: Dict[Any, Dict[int, tuple]] = {}
         
-        for c in y_train.unique():
+        for c in np.unique(y_train):
             feature_params[c] = {}
             subset = X_train[y_train == c]
-            for feature in X_train.columns:
-                mean = subset[feature].mean()
-                var = subset[feature].var()
-                feature_params[c][feature] = (mean, var)
+            for i in range(X_train.shape[1]):
+                mean = subset[:, i].mean()
+                var = subset[:, i].var()
+                feature_params[c][i] = (mean, var)
         
         return feature_params
     
